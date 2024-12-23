3eb00fd238e17898f6570a441ecd00ee2d46f13d
Jonathanrrs <yoni_12rrs@hotmail.com>
2024-12-23 17:01:12 -0600

    chat design

--- react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
+++ react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,16 @@
+import { GptMessage } from "../../components"
+import { MyMessage } from "../../components/chat-bubbles/MyMessage"
+
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Welcome */}
+          <GptMessage text="Hello, you can write your message in english and I will help you" />
+          <MyMessage text="Hi"/>
+        </div>
+      </div>
+    </div>
   )
 }
